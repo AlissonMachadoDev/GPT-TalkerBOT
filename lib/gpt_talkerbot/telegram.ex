6f50eeb31de7be31df1c890f4e9c0a53c9c31bcc
Alisson Machado <aamachado@inf.ufsm.br>
2025-12-08 16:20:20 -0300

    feat: add channel support

--- lib/gpt_talkerbot/telegram.ex
+++ lib/gpt_talkerbot/telegram.ex
@@ -6,6 +6,7 @@ defmodule GptTalkerbot.Telegram do
 
   def build_message(params) do
     params
+    |> format_params()
     |> Message.cast()
     |> case do
       %Ecto.Changeset{valid?: true} = changeset ->
@@ -33,4 +34,17 @@ defmodule GptTalkerbot.Telegram do
   def enqueue_processing!(%Message{} = m) do
     RMQPublisher.publish_message(m)
   end
+
+  defp format_params(%{"from" => %{"username" => "Channel_Bot"}} = message) do
+    name = message["sender_chat"]["title"] || "Channel Bot"
+    id = message["sender_chat"]["id"] || message["from"]["id"]
+    from = Map.put(message["from"], "first_name", name)
+    |> Map.put("id", id)
+    |> Map.put("username", "formatted_channel_bot")
+
+    message
+    |> Map.put("from", from)
+  end
+
+  defp format_params(params), do: params
 end
